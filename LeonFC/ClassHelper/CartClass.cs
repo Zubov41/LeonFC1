7e610675ce78c81edc21ac549323f718a4191011
123 <123123123>
2023-03-17 14:19:05 +0300

    Iconki

---
+++ LeonFC/ClassHelper/CartClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LeonFC.DataBase;
+
+namespace LeonFC.ClassHelper
+{
+    internal class CartClass
+    {
+        public static List<Service> serviceCart = new List<Service>();
+    }
+}
--- LeonFC/Windows/ServiceListWindow.xaml.cs
+++ LeonFC/Windows/ServiceListWindow.xaml.cs
@@ -42,6 +42,23 @@ namespace LeonFC.Windows
             //поиск
             serviceList = serviceList.Where(i => i.NameService.ToLower().Contains(TbSearch.Text.ToLower())).ToList();
 
+            // сортировка
+            switch (CmbSort.SelectedIndex)
+            {
+                case 0:
+                    serviceList = serviceList.OrderBy(i => i.IDService).ToList();
+                    break;
+                case 1:
+                    serviceList = serviceList.OrderBy(i => i.NameService).ToList();
+                    break;
+                case 2:
+                    serviceList = serviceList.OrderByDescending(i => i.NameService).ToList();
+                    break;
+                default:
+                    serviceList = serviceList.OrderBy(i => i.IDService).ToList();
+                    break;
+            }
+
             lvService.ItemsSource = serviceList;
         }
 
@@ -53,6 +70,7 @@ namespace LeonFC.Windows
             GetServiceList();
         }
 
+        // редактирование
         private void BtnEditProduct_Click(object sender, RoutedEventArgs e)
         {
             var button = sender as Button;
@@ -73,6 +91,31 @@ namespace LeonFC.Windows
         private void TbSearch_TextChanged(object sender, TextChangedEventArgs e)
         {
             GetServiceList();
+        }
+
+        private void CmbSort_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            GetServiceList();
+        }
+
+        private void BtnCartService_Click(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            if (button == null)
+            {
+                return;
+            }
+
+            var service = button.DataContext as Service;
+
+            ClassHelper.CartClass.serviceCart.Add(service);
+            MessageBox.Show($"Услуга {service.NameService.ToString()} добавлена");
+        }
+
+        private void BtnGoToCart_Click(object sender, RoutedEventArgs e)
+        {
+            CartWindow cartWindow = new CartWindow();
+            cartWindow.ShowDialog();
         }
     }
 }
