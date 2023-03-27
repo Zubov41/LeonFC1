4b9ef8b3f14fdbb172268e5302c45f54b4fdee47
123 <123123123>
2023-03-27 12:39:12 +0300

    deadline

---
+++ LeonFC/ClassHelper/UserClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeonFC.ClassHelper
+{
+    public class UserClass
+    {
+        public static DataBase.User AuthUser { get; set; }
+    }
+}
--- LeonFC/MainWindow.xaml.cs
+++ LeonFC/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace LeonFC
         public MainWindow()
         {
             InitializeComponent();
+            TxtNameUser.Text = "Пользователь " + ClassHelper.UserClass.AuthUser.Login + " " + ClassHelper.UserClass.AuthUser.Role.NameRole;
         }
     }
 }
